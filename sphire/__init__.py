a9dfb69002c61de3f134ba3a218cb3f120a6008b
phorvath <h.pijotr@gmail.com>
2019-05-20 15:33:53 +0200

    change source activate to conda activate introduce getCryoloEnvActivation ignore validateInstallation and _parseCryoloVersion

--- sphire/__init__.py
+++ sphire/__init__.py
@@ -32,7 +32,7 @@ import subprocess
 
 import pyworkflow.em
 import pyworkflow.utils as pwutils
-
+import pyworkflow as pw
 from sphire.constants import CRYOLO_GENMOD_VAR, CRYOLO_ENV_ACTIVATION
 
 _logo = "sphire_logo.png"
@@ -47,11 +47,15 @@ class Plugin(pyworkflow.em.Plugin):
     def _defineVariables(cls):
         # CRYOLO do NOT need EmVar because it uses a conda environment.
         cls._defineVar(CRYOLO_GENMOD_VAR, '')
-        cls._defineVar(CRYOLO_ENV_ACTIVATION, 'source activate cryolo')
+        cls._defineVar(CRYOLO_ENV_ACTIVATION, 'conda activate cryolo')
+
 
     @classmethod
     def getCryoloEnvActivation(cls):
-        return cls.getVar(CRYOLO_ENV_ACTIVATION)
+        activation = cls.getVar(CRYOLO_ENV_ACTIVATION)
+        scipionHome = pw.Config.SCIPION_HOME + os.path.sep
+
+        return activation.replace(scipionHome, "", 1)
 
     @classmethod
     def getCryoloGeneralModel(cls):
@@ -66,43 +70,44 @@ class Plugin(pyworkflow.em.Plugin):
             del environ['PYTHONPATH']
         return environ
 
-    @classmethod
-    def validateInstallation(cls):
-        """
-        Check we can activate the crYOLO environment using the provided
-        command via variable CRYOLO_ENV_ACTIVATION and the version can be
-        parsed.
-        """
-        errors = []
-        cls.__parseCryoloVersion()
-
-        if cls._cryoloVersion is None:
-            errors.append("crYOLO environment could not be activated.\n"
-                          "or the version could not be parsed. \n"
-                          "Using %s=%s" % (CRYOLO_ENV_ACTIVATION,
-                                           cls.getCryoloEnvActivation()))
-        elif not cls._cryoloVersionSupported:
-            errors.append("crYOLO version %s unsupported" % cls._cryoloVersion)
-
-        return errors
-
-    @classmethod
-    def __parseCryoloVersion(cls):
-        # If the version has not been detected, try to load the environment
-        if cls._cryoloVersionSupported is None:
-            try:
-                # check if is crYOLO is installed or not
-                cmd = cls.getCryoloEnvActivation()
-                cmd += '; pip list | grep cryolo'
-                p = subprocess.Popen(["bash", "-c", cmd], env=cls.getEnviron(),
-                                     stdin=subprocess.PIPE, stdout=subprocess.PIPE)
-                output, err = p.communicate()
-                cls._cryoloVersion = output.split()[1]
-                from pkg_resources import parse_version
-                cls._cryoloVersionSupported = parse_version(cls._cryoloVersion) >= parse_version("1.2")
-            except Exception as e:
-                cls._cryoloVersion = None
-                cls._cryoloVersionSupported = False
+    #Ignore validateInstallation and __parseCryoloVersion due to the decrease in performance. Although they are functional.
+    # @classmethod
+    # def validateInstallation(cls):
+    #     """
+    #     Check we can activate the crYOLO environment using the provided
+    #     command via variable CRYOLO_ENV_ACTIVATION and the version can be
+    #     parsed.
+    #     """
+    #     errors = []
+    #     cls.__parseCryoloVersion()
+    #
+    #     if cls._cryoloVersion is None:
+    #         errors.append("crYOLO environment could not be activated.\n"
+    #                       "or the version could not be parsed. \n"
+    #                       "Using %s=%s" % (CRYOLO_ENV_ACTIVATION,
+    #                                        cls.getCryoloEnvActivation()))
+    #     elif not cls._cryoloVersionSupported:
+    #         errors.append("crYOLO version %s unsupported" % cls._cryoloVersion)
+    #
+    #     return errors
+    #
+    # @classmethod
+    # def __parseCryoloVersion(cls):
+    #     # If the version has not been detected, try to load the environment
+    #     if cls._cryoloVersionSupported is None:
+    #         try:
+    #             # check if is crYOLO is installed or not
+    #             cmd = cls.getCryoloEnvActivation()
+    #             cmd += '; pip list | grep cryolo'
+    #             p = subprocess.Popen(["bash", "-c", cmd], env=cls.getEnviron(),
+    #                                  stdin=subprocess.PIPE, stdout=subprocess.PIPE)
+    #             output, err = p.communicate()
+    #             cls._cryoloVersion = output.split()[1]
+    #             from pkg_resources import parse_version
+    #             cls._cryoloVersionSupported = parse_version(cls._cryoloVersion) >= parse_version("1.2")
+    #         except Exception as e:
+    #             cls._cryoloVersion = None
+    #             cls._cryoloVersionSupported = False
 
     @classmethod
     def runCryolo(cls, protocol, program, args, cwd=None):
